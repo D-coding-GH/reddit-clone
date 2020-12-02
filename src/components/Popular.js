152815e7e167ef1f26b1237a7b5fb1c2e0d11409
Alex Phimister <wphimmy@gmail.com>
2020-12-02 17:16:13 +0000

    Placeholders for central popular posts content

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,15 +1,19 @@
 import React from "react";
-
+import Sortbar from "./Sortbar";
+import "./Popular.css";
 const Popular = () => {
   return (
-    <div>
-      <h3>Popular posts</h3>
-      <div>
-        This Div contains the central content
-        <div>Sort bar here </div>
-        <div>This div is for our content, including votes and comments</div>
+    <div id="popular">
+      <div id="central">
+        {/*this div contains all the central content*/}
+        <h3>Popular posts</h3>
+        <Sortbar />
+        <div id="posts">
+          This div is for our content, including votes and comments
+        </div>
       </div>
-      <div>Sidebar content</div>
+      <div id="sidebar">Sidebar content</div>
+      {/*this div contains all the sidebar content*/}
     </div>
   );
 };
