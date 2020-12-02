4bbf01e23f49f07fe12bcb4f9dc1fa33f50e8776
Code Nation <codenation@unknown-38:f9:d3:56:b3:31.lan>
2020-12-02 17:27:22 +0000

    tile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Trending from './components/Trending';
 import "./App.css";
 
 function App() {
   return <div className="App">Hello World danny version</div>;
 }
 
-export default App;
+export default Trending;
---
+++ src/components/Trending.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import "./Trending.css"
+
+
+class Trending extends React.Component {///...danny version
+    render() {
+      return (
+  
+        <div>
+          <div className="container">
+           
+           <div id = "card1"    >
+            <Card 
+              headline="Vaccine Roll-Out expected in weeks"
+              
+              topic=" international news and more &amp; corona"
+            />
+           </div>
+
+
+            <div  id = "card2"    >
+            <Card 
+              headline="Hamilton replaced for next F1"
+              topic="sports and more"
+           
+            />
+            </div>
+
+             <div id = "card3" >
+               <Card 
+              headline="PC building walk through"
+              topic="tech news and more &amp; TECH"
+         
+            />
+            </div>
+            
+            
+             <div id = "card4"  > 
+             <Card 
+              headline="trump still wont leave"
+              topic="USA and more &amp; politics"
+            
+            /> 
+            </div>
+
+
+
+          </div>
+  
+          
+          
+          
+       </div>
+         
+  
+      );
+    }
+  }
+  const Card = (props) => {
+    return (
+      <div className="card-wrapper">
+        <img src={props.image} alt="" className = "photo1"/>
+        <h3>{props.headline}</h3>
+        <h4>{props.topic.toUpperCase()}</h4>
+      </div>
+    );
+  };
+
+export default Trending;
